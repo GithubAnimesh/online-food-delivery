058d9ab084f4e2c9284234d93cb5dd2de7e28a22
Animesh Singh <animesh.singh@gmail.com>
2024-12-13 06:20:16 +0530

    Installation and setup completed

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "parent-heading" }, "Hello from parent"),
-    React.createElement("h1", { id: "parent-heading2" }, "Hello from parent2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "parent-heading" }, "Hello from parent"),
-    React.createElement("h1", { id: "parent-heading2" }, "Hello from parent2"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement(
   "h1",
-  { id: "heading" },
+  { id: "head" },
   "Hello word from React"
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
